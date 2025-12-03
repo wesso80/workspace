d96ffab9a9ffc4182d60e51ba5e805e0a91ff6e3
wesso80 <bradleywessling@yahoo.com.au>
2025-12-03 12:54:25 +1100

    Remove babel-plugin-module-resolver (not needed, all @/ aliases removed)

--- native-app/babel.config.js
+++ native-app/babel.config.js
@@ -3,15 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
-        },
-      ],
       'react-native-reanimated/plugin',
     ],
   };
