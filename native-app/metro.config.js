f735913b0676f1395f6daf926e28e59b41cc8dcb
wesso80 <bradleywessling@yahoo.com.au>
2025-12-03 14:43:46 +1100

    Add empty shims for Node.js modules (tty, fs, net, etc)

--- native-app/metro.config.js
+++ native-app/metro.config.js
@@ -1,11 +1,25 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver = config.resolver || {};
+// Create empty shim path
+const emptyShim = path.resolve(__dirname, 'shims/empty.js');
+
 config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  tty: require.resolve('tty-browserify'),
+  ...config.resolver.extraNodeModules,
+  tty: emptyShim,
+  fs: emptyShim,
+  net: emptyShim,
+  dns: emptyShim,
+  child_process: emptyShim,
+  stream: emptyShim,
+  os: emptyShim,
+  path: emptyShim,
+  zlib: emptyShim,
+  http: emptyShim,
+  https: emptyShim,
+  crypto: emptyShim,
 };
 
 module.exports = config;
