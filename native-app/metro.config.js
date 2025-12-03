aaec671388bfa4cc483be07d710176b185359aed
wesso80 <bradleywessling@yahoo.com.au>
2025-12-03 14:35:51 +1100

    Add react-dom, react-native-web, tty-browserify and fix metro config

--- native-app/metro.config.js
+++ native-app/metro.config.js
@@ -2,13 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === 'tty' || moduleName === 'fs' || moduleName === 'net' || moduleName === 'dns' || moduleName === 'child_process') {
-    return {
-      type: 'empty',
-    };
-  }
-  return context.resolveRequest(context, moduleName, platform);
+config.resolver = config.resolver || {};
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  tty: require.resolve('tty-browserify'),
 };
 
 module.exports = config;
